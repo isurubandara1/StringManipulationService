2ae3587ef403d7fc3491d8425c172efdb05eb1a4
isurubandara1 <isurumbandara@gmail.com>
2025-03-21 14:55:47 +0530

    second commit

--- src/com/example/service/ServicePublisher.java
+++
@@ -1,8 +0,0 @@
-import javax.xml.ws.Endpoint;
-
-public class ServicePublisher {
-    public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8080/StringManipulationService", new StringManipulationServiceImpl());
-        System.out.println("Service is running...");
-    }
-}
